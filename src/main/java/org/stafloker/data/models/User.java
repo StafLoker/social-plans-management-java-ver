7fa177784d6820814644284f3ccd5ffd60063fa5
StafLoker <staf.loker@gmail.com>
2023-12-27 14:50:43 +0300

    feat: complete User

--- src/main/java/org/stafloker/data/models/User.java
+++ src/main/java/org/stafloker/data/models/User.java
@@ -1,5 +1,45 @@
 package org.stafloker.data.models;
 
-public class User {
+import lombok.*;
+import org.stafloker.data.models.exceptions.InvalidAttributeException;
 
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    private static final int MIN_AGE = 14;
+    private static final int MAX_AGE = 100;
+    private static final int MIN_LONG_PASSWORD = 3;
+
+    @Id
+    @GeneratedValue
+    @Column
+    private Integer id;
+    @Column(nullable = false, unique = true)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String mobile;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private Integer age;
+
+    public void setPassword(String password) {
+        if (password.length() < MIN_LONG_PASSWORD) {
+            throw new InvalidAttributeException("Password has less than three characters: " + password);
+        }
+        this.password = password;
+    }
+
+    public void setAge(Integer age) {
+        if (age < MIN_AGE || age > MAX_AGE) {
+            throw new InvalidAttributeException("Age is not in your range [" + MIN_AGE + "," + MAX_AGE + "]: " + age);
+        }
+        this.age = age;
+    }
 }
