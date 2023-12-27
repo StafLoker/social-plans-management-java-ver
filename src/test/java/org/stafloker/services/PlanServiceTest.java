b2564f4b27a336b8bb3660f208175b9957595f64
StafLoker <staf.loker@gmail.com>
2023-12-27 23:48:27 +0300

    feat: tests of services

---
+++ src/test/java/org/stafloker/services/PlanServiceTest.java
@@ -0,0 +1,52 @@
+package org.stafloker.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlanServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void addActivity() {
+    }
+
+    @Test
+    void enrollSubscriber() {
+    }
+
+    @Test
+    void cost() {
+    }
+
+    @Test
+    void duration() {
+    }
+
+    @Test
+    void availablePlans() {
+    }
+
+    @Test
+    void subscribedPlans() {
+    }
+
+    @Test
+    void costRangePlans() {
+    }
+
+    @Test
+    void weekendPlans() {
+    }
+
+    @Test
+    void plansWithKeyword() {
+    }
+}
