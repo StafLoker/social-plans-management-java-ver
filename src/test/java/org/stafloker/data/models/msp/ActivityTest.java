d3d079add23f728107c922211fc867cbfbcab0da
StafLoker <staf.loker@gmail.com>
2023-12-27 17:26:45 +0300

    feat: tests of models

---
+++ src/test/java/org/stafloker/data/models/msp/ActivityTest.java
@@ -0,0 +1,16 @@
+package org.stafloker.data.models.msp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActivityTest {
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void setCapacity() {
+    }
+}
