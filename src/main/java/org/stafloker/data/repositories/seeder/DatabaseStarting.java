3d5fc58f416ca3dcb1583f05d77e6ffaae66201e
StafLoker <staf.loker@gmail.com>
2023-12-28 18:34:38 +0300

    feat: seeder

---
+++ src/main/java/org/stafloker/data/repositories/seeder/DatabaseStarting.java
@@ -0,0 +1,33 @@
+package org.stafloker.data.repositories.seeder;
+
+import org.apache.logging.log4j.LogManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+import org.stafloker.data.models.User;
+import org.stafloker.data.repositories.UserRepository;
+
+@Repository
+public class DatabaseStarting {
+    private final UserRepository userRepository;
+    private final String name;
+    private final String pass;
+
+    @Autowired
+    public DatabaseStarting(@Value("${spm.root.name}") String name,
+                            @Value("${spm.root.pass}") String pass, UserRepository userRepository) {
+        this.name = name;
+        this.pass = pass;
+        this.userRepository = userRepository;
+        this.initialize();
+    }
+
+    void initialize() {
+        LogManager.getLogger(this.getClass()).warn("------- Finding Root -------------");
+        if (this.userRepository.searchByName(this.name).isEmpty()) {
+            User user = User.builder().name(this.name).password(this.pass).age(21).mobile("111111111").build();
+            this.userRepository.create(user);
+            LogManager.getLogger(this.getClass()).warn("------- Created Root -------------");
+        }
+    }
+}
