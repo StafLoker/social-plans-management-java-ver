8312c13b576af63573774931775a6566db0902a3
StafLoker <staf.loker@gmail.com>
2024-01-05 20:15:03 +0300

    feat: daos tests

--- src/test/java/org/stafloker/data/daos/PlanRepositoryTest.java
+++ src/test/java/org/stafloker/data/daos/PlanRepositoryTest.java
@@ -1,8 +1,68 @@
 package org.stafloker.data.daos;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.stafloker.TestConfig;
+import org.stafloker.data.models.spm.Plan;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestConfig
 class PlanRepositoryTest {
+
+    @Autowired
+    private PlanRepository planRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void testCreate() {
+        Plan plan = Plan.builder().name("Test").date(LocalDateTime.now().plusDays(93).plusHours(24).plusMinutes(60)).meetingPlace("Sol, Madrid").build();
+        plan.setOwner(this.userRepository.read(1L).get());
+        plan = this.planRepository.create(plan);
+        assertTrue(this.planRepository.read(plan.getId()).isPresent());
+        this.planRepository.deleteById(plan.getId());
+    }
+
+    @Test
+    void testUpdate() {
+        Plan plan = this.planRepository.read(1L).get();
+        plan.setName("Test");
+        this.planRepository.update(plan);
+        assertEquals("Test", this.planRepository.read(plan.getId()).get().getName());
+        plan.setName("Plan0, noon, pizza + cinema + beers");
+        this.planRepository.update(plan);
+    }
+
+    @Test
+    void testRead() {
+        assertTrue(this.planRepository.read(1L).isPresent());
+
+        assertFalse(this.planRepository.read(50L).isPresent());
+    }
+
+    @Test
+    void testDelete() {
+        Plan plan = Plan.builder().name("Test").date(LocalDateTime.now().plusDays(93).plusHours(24).plusMinutes(60)).meetingPlace("Sol, Madrid").build();
+        plan.setOwner(this.userRepository.read(1L).get());
+        plan = this.planRepository.create(plan);
+        this.planRepository.deleteById(plan.getId());
+        assertFalse(this.planRepository.read(plan.getId()).isPresent());
+        this.planRepository.deleteById(plan.getId());
+    }
+
+    @Test
+    void testGetAll() {
+        Plan temp = Plan.builder().name("Test").date(LocalDateTime.now().plusDays(93).plusHours(24).plusMinutes(60)).meetingPlace("Sol, Madrid").build();
+        temp.setOwner(this.userRepository.read(1L).get());
+        Plan plan = this.planRepository.create(temp);
+        List<Plan> list = this.planRepository.findAll();
+        assertTrue(list.contains(plan));
+        assertTrue(list.contains(this.planRepository.read(1L).get()));
+        this.planRepository.deleteById(plan.getId());
+    }
 
 }
--- src/test/java/org/stafloker/data/daos/UserRepositoryTest.java
+++ src/test/java/org/stafloker/data/daos/UserRepositoryTest.java
@@ -1,16 +1,71 @@
 package org.stafloker.data.daos;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.stafloker.TestConfig;
+import org.stafloker.data.models.User;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestConfig
 class UserRepositoryTest {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
-    void findByName() {
+    void testFindByName() {
+        assertTrue(this.userRepository.findByName("root").isPresent());
+
+        assertFalse(this.userRepository.findByName("Test").isPresent());
     }
 
     @Test
-    void findByMobile() {
+    void testFindByMobile() {
+        assertTrue(this.userRepository.findByMobile("111111111").isPresent());
+
+        assertFalse(this.userRepository.findByMobile("111111112").isPresent());
     }
+
+    @Test
+    void testCreate() {
+        User user = this.userRepository.create(User.builder().name("test").password("test").age(19).mobile("123456789").build());
+        assertTrue(this.userRepository.findByName(user.getName()).isPresent());
+        this.userRepository.deleteById(user.getId());
+    }
+
+    @Test
+    void testUpdate() {
+        User user = this.userRepository.read(6L).get();
+        user.setName("Test");
+        this.userRepository.update(user);
+        assertTrue(this.userRepository.findByName("Test").isPresent());
+        user.setName("User5");
+        this.userRepository.update(user);
+    }
+
+    @Test
+    void testRead() {
+        assertTrue(this.userRepository.read(1L).isPresent());
+
+        assertFalse(this.userRepository.read(50L).isPresent());
+    }
+
+    @Test
+    void testDelete() {
+        User user = this.userRepository.create(User.builder().name("test").password("test").age(19).mobile("123456789").build());
+        this.userRepository.deleteById(user.getId());
+        assertFalse(this.userRepository.findByName(user.getName()).isPresent());
+    }
+
+    @Test
+    void testGetAll() {
+        User user = this.userRepository.create(User.builder().name("test").password("test").age(19).mobile("123456789").build());
+        List<User> list = this.userRepository.findAll();
+        assertTrue(list.contains(user));
+        this.userRepository.deleteById(user.getId());
+    }
+
 }
